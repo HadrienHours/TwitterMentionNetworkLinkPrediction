7a54f8d8871bf847ff9177affe794703aef6ffa6
HadrienHours <hadrien.hours@eurecom.fr>
2016-07-18 18:00:41 +0200

    removed panda dataframe index and float to int

--- mention_network_study.dir/clustering.dir/groupinghashtags.dir/script_reformat_hashlabels.sh
+++ mention_network_study.dir/clustering.dir/groupinghashtags.dir/script_reformat_hashlabels.sh
@@ -22,14 +22,14 @@ then
     mkdir $sizdir
 fi
 
-for fin in $dirin/*.csv
+for fin in $dirin/*.tgz
 do
     echo -e "Treating file \e[3m$fin\e[0m"
-    fout=$(echo $fin | sed -re 's/\.csv//1')_REFORMATTED.tgz
-    fstat=$sizdir/$(basename $fin | sed -re 's/\.csv//1')_CLUSTERSIZES.tgz
-    #remove header and non core points, sort hashes for future join
-    zcat $fin | sed 1d | egrep -v ",$" | sort -t  , -k1,1 | gzip > $fout
-    zcat $fin | sed 1d | egrep -v ",$" | cut -d , -f2 | sort -n | uniq -c | awk 'BEGIN{OFS=","}{print $2,$1}' | sort -n -t , -k1,1 | gzip > $fstat
+    fout=$(echo $fin | sed -re 's/\.tgz//1')_REFORMATTED.tgz
+    fstat=$sizdir/$(basename $fin | sed -re 's/\.tgz//1')_CLUSTERSIZES.tgz
+    #remove index, non core points, convert float to int and sort hashes for future join
+    zcat $fin | cut -d , -f2- | egrep -v ",$" | sed -re 's/\.0$//1' | sort -t  , -k1,1 | gzip > $fout
+    zcat $fin | cut -d , -f2- | egrep -v ",$" | sed -re 's/\.0$//1' | cut -d , -f2 | sort -n | uniq -c | awk 'BEGIN{OFS=","}{print $2,$1}' | sort -n -t , -k1,1 | gzip > $fstat
     mv $fin $auxdir
     echo -e "\e[3m$fin\e[0m moved to \e[4m$auxdir\e[0m\n"
 done
