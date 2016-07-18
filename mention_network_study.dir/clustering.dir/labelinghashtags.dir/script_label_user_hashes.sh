e702dd9ca5c1228eff48f29df392f7bb5d2cee29
HadrienHours <hadrien.hours@eurecom.fr>
2016-07-18 18:02:12 +0200

    take into account compressed files

--- mention_network_study.dir/clustering.dir/labelinghashtags.dir/script_label_user_hashes.sh
+++ mention_network_study.dir/clustering.dir/labelinghashtags.dir/script_label_user_hashes.sh
@@ -25,9 +25,9 @@ then
     mkdir -p $sortlabhsh
 fi
 
-for filelab in $dirlabs/*.csv
+for filelab in $dirlabs/*.tgz
 do
-    period=$(basename $filelab | egrep -o "[0-9]{10}")
+    period=$(basename $filelab | egrep -o "[0-9]{10}\-[0-9]{10}")
     fileuserhsh=$(ls $diruserhsh | grep "$period")
     if [ -z $fileuserhsh ]
     then
@@ -38,11 +38,11 @@ do
 
     echo -e "For period \e[1m$period\e[0m find matching file: \e[3m$fileuserhsh\e[0m"
 
-    f1=$sortuserhsh/$(basename $fileuserhsh | sed -re 's/\.csv//1')_SORTED.csv
-    f2=$sortlabhsh/$(basename $filelab | sed -re 's/\.csv//1')_SORTED.csv
-    cat $filelab | sort -t , -k1,1 > $f2
-    cat $diruserhsh/$fileuserhsh | sort -t , -k2,2 > $f1
-    fout=$dirout/$(basename $fileuserhsh | sed -re 's/\.csv//1')_LABELED_DBSCAN.csv
-    join -t , -12 -21 $f1 $f2 | cut -d , -f2- > $fout
+    f1=$sortuserhsh/$(basename $fileuserhsh | sed -re 's/\.tgz//1')_SORTED.tgz
+    f2=$sortlabhsh/$(basename $filelab | sed -re 's/\.tgz//1')_SORTED.tgz
+    zcat $filelab | sort -t , -k1,1 | gzip > $f2
+    zcat $diruserhsh/$fileuserhsh | sort -t , -k2,2 | gzip > $f1
+    fout=$dirout/$(basename $fileuserhsh | sed -re 's/\.tgz//1')_LABELED_DBSCAN.tgz
+    join -t , -12 <(zcat $f1) -21 <(zcat $f2) | cut -d , -f2- | gzip > $fout
     echo -e "File treated, result stored in \e[3m$fout\e[0m\n"
 done
